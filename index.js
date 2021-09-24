f6919567a3df9b3ac46c80b8bdf437c651bd4fbc
Yeison <enmanuelquiterios@gmail.com>
2021-09-24 13:21:43 -0400

    commit tree 52dbb9dc3d9a23133af3934cb0723b7cee846c53

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution here
+function findMatching(drivers, string) {
+  let filterResult = drivers.filter((name) => {
+    if (
+      name.toLowerCase() === string.toLowerCase() ||
+      name.toUpperCase() === string.toUpperCase()
+    ) {
+      let founds = [];
+      founds.push(name);
+      return founds;
+    }
+  });
+  return filterResult;
+}
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+
+findMatching(drivers, "Sammy");
+
+//---------------------------------------------
+function fuzzyMatch(drivers, string) {
+  let match = true;
+  let word = drivers.filter((letter) => letter[0] === string[0]);
+  if (match) {
+    return word;
+  } else if (!match) {
+    return [];
+  }
+}
+
+function matchName(drivers, string) {
+  let match = true;
+  if (match) {
+    let found = drivers.filter((name) => name === string.name);
+    return found;
+  } else {
+    return "No Name Found!";
+  }
+}
